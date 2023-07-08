57122622c316ee432959116743e2d6de95e8b2b9
Byeongcheol Kim <kbc20000@gmail.com>
2023-07-08 20:33:46 +0900

    Initial Commit

---
+++ meeco/meecoApp.swift
@@ -0,0 +1,17 @@
+//
+//  meecoApp.swift
+//  meeco
+//
+//  Created by 김병철 on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct meecoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meecoUITests/meecoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  meecoUITestsLaunchTests.swift
+//  meecoUITests
+//
+//  Created by 김병철 on 2023/07/08.
+//
+
+import XCTest
+
+final class meecoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
